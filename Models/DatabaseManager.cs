00b79dbd0967db39d12830730aa20f17bc6f3d92
mathi <math260d@zbc.dk>
2021-07-23 20:19:39 +0200

    added db con

--- Models/DatabaseManager.cs
+++ Models/DatabaseManager.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Text;
-
 
 namespace BFSMonitoringWebService1.Models
 {
